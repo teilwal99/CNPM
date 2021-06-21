466dbdb6e0cb0ce9732909190b541b04162b1f80
teilwal99 <44613711+teilwal99@users.noreply.github.com>
2021-06-21 23:32:11 +0700

    included

--- session.php
+++ session.php
@@ -3,11 +3,9 @@ include('config.php');
 session_start();
 
 $user_check = $_SESSION['userid'];
-
 $ses_sql = mysqli_query($db, "select * from user where id = '$user_check' ");
 
 $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
-
 
 if (isset($_SESSION['login_user'])) {
    $userid_session = $row['id'];
